df361c530439c8b9f83c03c59cf42dbee63bbfd1
mikelgaray <garaymikelaritz@gmail.com>
2025-01-23 12:32:47 +0100

    Actualizado_Mikel

--- src/clases/GestorAlumnos.java
+++ src/clases/GestorAlumnos.java
@@ -1,5 +1,18 @@
 package clases;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GestorAlumnos {
+	   private List<Alumno> alumnos = new ArrayList<>();
+
+	    public void añadirAlumno(Alumno alumno) {
+	        alumnos.add(alumno);
+	        System.out.println("Alumno añadido: " + alumno);
+	    }
+
+	    public List<Alumno> getAlumnos() {
+	        return alumnos;
+	    }
 
 }
