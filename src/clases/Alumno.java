fd07b94e0e971edc51f12740f86098982cadfe93
mikelgaray <garaymikelaritz@gmail.com>
2025-01-23 12:11:32 +0100

    Segundo parejas

---
+++ src/clases/Alumno.java
@@ -0,0 +1,17 @@
+package clases;
+
+public class Alumno {
+	  private String nombre;
+	    private int edad;
+
+	    public Alumno(String nombre, int edad) {
+	        this.nombre = nombre;
+	        this.edad = edad;
+	    }
+
+	    @Override
+	    public String toString() {
+	        return "Alumno{" + "nombre='" + nombre + '\'' + ", edad=" + edad + '}';
+	    }
+
+}
---
+++ src/principal/Principal.java
@@ -0,0 +1,11 @@
+package principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Menu menu=new Menu();
+		menu.mostrarMenu();
+	}
+
+}
